1a3c9aea88e2047410e909647e38613cae9230cc
unbotcomotu <a20213852@pucp.edu.pe>
2024-05-09 19:42:44 -0500

    Initial commit

---
+++ src/main/java/com/example/lab520213852/Lab520213852Application.java
@@ -0,0 +1,13 @@
+package com.example.lab520213852;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520213852Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab520213852Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab520213852/Lab520213852ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab520213852;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520213852ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
